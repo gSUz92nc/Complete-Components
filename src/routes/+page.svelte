5ea85e16c84363dca359d74d5f4592c6b523ca04
gSUz92nc <99101158+gSUz92nc@users.noreply.github.com>
2024-05-21 15:20:47 +0100

    Update +page.svelte

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -22,5 +22,7 @@
   <div class="my-auto flex justify-center flex-col">
     <h1 class="text-4xl font-bold text-center">Welcome!</h1>
     <a class="btn btn-primary mt-8" href={(loggedIn ? '/dashboard' : '/login')}>Get Started</a>
+    <p class="text-center mt-4">This project is paused. For more info check our github repository</p>
+    <a class="link text-center mt-2" href="https://github.com/gSUz92nc/Complete-Components">https://github.com/gSUz92nc/Complete-Components</a>
   </div>
 </div>
